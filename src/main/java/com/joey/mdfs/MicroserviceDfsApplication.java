d30296fde091cdb5b2d3c86b2fdc6d434a35219b
SinestroEdmonce <867208887@qq.com>
2019-06-18 23:57:51 +0800

    Update: 1. Update Initialized project for MDFS

---
+++ src/main/java/com/joey/mdfs/MicroserviceDfsApplication.java
@@ -0,0 +1,13 @@
+package com.joey.mdfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceDfsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceDfsApplication.class, args);
+	}
+
+}
